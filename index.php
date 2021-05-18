9a9f821b8954588c3e40716eadc483571680b53e
strobosaur <69212054+strobosaur@users.noreply.github.com>
2021-05-18 11:06:06 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<?php
+  session_start();
+?>
+
+<html>
+
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="./css/styles.css">
+  <script src="./js/functions.js"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
