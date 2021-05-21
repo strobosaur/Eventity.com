9493d5cc95c1d25f4cc448b3de19e713a434dd9c
strobosaur <strobosaur@gmail.com>
2021-05-21 13:29:58 +0200

    basic layout for index.php

--- include/views/_header.php
+++ include/views/_header.php
@@ -10,7 +10,6 @@
   <title></title>
   <meta charset="utf-8">
   <link rel="stylesheet" href="./css/styles.css">
-  <script src="./js/functions.js"></script>
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -2,6 +2,12 @@
   include_once './include/views/_header.php';
 ?>
 
+<div class="left-field" id="left-field">
+</div>
+
+<div class="event-list" id="event-list">
+</div>
+
 <?php
   include_once './include/views/_footer.php';
 ?>
